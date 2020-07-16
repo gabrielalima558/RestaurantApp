15b6d773610e79078539ab136432e650ec1f269e
Araujo <gabriela.araujo@capgemini.com>
2020-07-16 15:26:38 -0300

    Instrumented test to button items

--- app/src/androidTest/java/com/example/deliverytest/ListMenuActivityTest.kt
+++ app/src/androidTest/java/com/example/deliverytest/ListMenuActivityTest.kt
@@ -1,6 +1,7 @@
 package com.example.deliverytest
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -29,5 +30,10 @@ class ListMenuActivityTest {
             )
         )
         onView(withId(R.id.btn_items)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun shouldOpenCartActivity_whenClickItemButton(){
+        onView(withId(R.id.btn_items)).check(matches(isDisplayed())).perform(click())
     }
 }
